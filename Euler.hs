97f6c5ca77a94aa58e7e41d0b2c3390e1dc4e55f
Agam <agam.brahma@gmail.com>
2010-06-27 00:20:59 -0700

    Working solution to problem 1

--- Euler.hs
+++ Euler.hs
@@ -1,19 +1,19 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
-module Euler where
-import System.IO
+module Main where
 
 -- Current Project Euler max-project-number
---MAX_PROBLEM_NUM = 1 :: Integer
+max_problem_num :: Integer
+max_problem_num = 1
 
 -- Problem 1 - Add all the natural numbers below one thousand that are multiples of 3 or 5.
-problem1 :: (Integer a)
-problem1 = sum [x | x <- [1..1000], x `mod` 3 == 0, x `mod` 5 == 0]
+problem1 :: Integer
+problem1 = sum (filter (\x -> (x `mod` 3 == 0 || x `mod` 5 == 0)) [1 .. 999])
 
 -- Add new problems to this list as they are created
-eulerEval :: (Integer a) => a -> a
+eulerEval :: Integer -> Integer
 eulerEval 1 = problem1
 eulerEval _ = 0
 
 -- | main, do Nothing
 main :: IO()
-main = map print $ eulerEval [x | x <- [1..], x <= 1]
+main = mapM_ print (map eulerEval [1 .. max_problem_num])
