e95da99d197a04cefdb53ee779924ae358f836a6
Agam Brahma <agam.brahma@gmail.com>
2010-06-29 14:51:54 -0700

    Working solution to Problem 4

--- Euler.hs
+++ Euler.hs
@@ -1,9 +1,10 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
 module Main where
+import List
 
 -- Current Project Euler max-project-number
 max_problem_num :: Integer
-max_problem_num = 2
+max_problem_num = 4
 
 -- Problem 1 - Add all the natural numbers below one thousand that are multiples of 3 or 5.
 problem1 :: Integer
@@ -16,10 +17,31 @@ fibonacci_numbers = 0 : 1 : zipWith (+) fibonacci_numbers (tail fibonacci_number
 problem2 :: Integer
 problem2 = sum (fst (span (< 4000000) (filter even fibonacci_numbers)))
 
+-- Problem 4 -- Find the largest palindrome made from the product of two 3-digit numbers.
+digitArray :: Integer -> [Integer] -> [Integer]
+digitArray n arr
+	| n `div` 10 > 0 = digitArray (n `div` 10) ((n `rem` 10):arr)
+	| otherwise = ((n `rem` 10):arr)
+
+isMirror :: [Integer] -> Bool
+isMirror arr
+	| (length(arr) == 0) || (length(arr) == 1) = True
+	| head(arr) == head(reverse(arr)) = isMirror (drop 1 (reverse (drop 1 arr)))
+	| otherwise = False
+
+isPalindrome :: Integer -> Bool
+isPalindrome n = isMirror (digitArray n [])
+
+largest_palindrome :: Integer
+largest_palindrome = head( filter isPalindrome (reverse (sort productOfAllThreeDigitNumbers)))
+	where
+	productOfAllThreeDigitNumbers = [ x * y | x <- [999, 998 .. 100], y <- [999, 998 .. 100]]
+
 -- Add new problems to this list as they are created
 eulerEval :: Integer -> Integer
 eulerEval 1 = problem1
 eulerEval 2 = problem2
+eulerEval 4 = largest_palindrome
 eulerEval _ = 0
 
 -- | main, do Nothing
