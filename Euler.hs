e9f5d00d69875e686112e67f9775c96c6e3e265e
Agam Brahma <agam.brahma@gmail.com>
2010-07-09 22:33:09 -0700

    Working solution to Problem 11

--- Euler.hs
+++ Euler.hs
@@ -119,11 +119,11 @@ four_num_mat = [[08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12,
 		[20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54],
 		[01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48]]
 
-mat_element :: [[Integer]] -> Integer -> Integer -> Integer
+mat_element :: [[Integer]] -> Int -> Int -> Integer
 mat_element [] _ _ = error "Empty matrix passed in"
 mat_element mat i j = (mat !! i) !! j
 
-compute_dir_prod :: [[Integer]] -> Integer -> Integer -> Integer
+compute_dir_prod :: [[Integer]] -> Int -> Int -> Integer
 compute_dir_prod [] _ _ = error "Empty matrix !"
 compute_dir_prod mat i j = maximum [
 		product [mat_element mat i k | k <- [j-3 .. j]],
