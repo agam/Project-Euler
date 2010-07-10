d9695941dacaf51b861bdde0d2e5865c28056ead
Agam Brahma <agam.brahma@gmail.com>
2010-07-09 21:29:51 -0700

    Non working state for solution 11

--- Euler.hs
+++ Euler.hs
@@ -4,7 +4,7 @@ import List
 
 -- Current Project Euler max-project-number
 max_problem_num :: Integer
-max_problem_num = 10 
+max_problem_num = 11
 
 -- Problem 1 - Add all the natural numbers below one thousand that are multiples of 3 or 5.
 problem1 :: Integer
@@ -96,6 +96,55 @@ sum_primes runningsum prod (number:numbers)
 prime_sum :: Integer
 prime_sum = sum_primes 0 1 [2..2000000]
 
+-- Problem 11 -- Find the 4 digits with the largest product in any direction
+four_num_mat :: [[Integer]]
+four_num_mat = [[08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08],
+		[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00],
+		[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65],
+		[52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91],
+		[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+		[24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+		[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+		[67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21],
+		[24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+		[21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95],
+		[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92],
+		[16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57],
+		[86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+		[19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40],
+		[04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+		[88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+		[04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36],
+		[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16],
+		[20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54],
+		[01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48]]
+
+mat_element :: [[Integer]] -> Integer -> Integer -> Integer
+mat_element [] _ _ = error "Empty matrix passed in"
+mat_element mat i j = (mat !! i) !! j
+
+compute_dir_prod :: [[Integer]] -> Integer -> Integer -> Integer
+compute_dir_prod [] _ _ = error "Empty matrix !"
+compute_dir_prod mat i j = maximum [
+		product [mat_element mat i k | k <- [j-3 .. j]],
+		product [mat_element mat i k | k <- [j .. j+3]],
+		product [mat_element mat k j | k <- [i-3 .. i]],
+		product [mat_element mat k j | k <- [i .. i+3]],
+		product [mat_element mat (i-k) (j-k) | k <- [0 .. 3]],
+		product [mat_element mat (i+k) (j+k) | k <- [0 .. 3]],
+		product [mat_element mat (i-k) (j+k) | k <- [0 .. 3]],
+		product [mat_element mat (i+k) (j-k) | k <- [0 .. 3]]
+		]
+
+get_all_prod :: [[Integer]] -> [[Integer]]
+get_all_prod mat = [[ compute_dir_prod mat i j | j <- [3 .. ((length (mat !! i)) - 4)]] | i <- [3.. ((length mat) - 4)]]
+
+largest_mat_element :: [[Integer]] -> Integer
+largest_mat_element mat = maximum (map maximum mat)
+
+four_num_prod :: Integer
+four_num_prod = largest_mat_element (get_all_prod four_num_mat)
+
 -- Add new problems to this list as they are created
 eulerEval :: Integer -> Integer
 eulerEval 1 = problem1
@@ -107,6 +156,7 @@ eulerEval 7 = prime_10001
 eulerEval 8 = digitProduct
 eulerEval 9 = findPythTriplet
 eulerEval 10 = prime_sum
+eulerEval 11 = four_num_prod
 eulerEval _ = 0
 
 -- | main, print out all known solutions
