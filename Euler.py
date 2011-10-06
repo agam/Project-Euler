9b7c01e26a2dadd887eb4b4f8b102b24228624a2
Agam <agam.brahma@gmail.com>
2011-10-05 21:00:46 -0700

    ...

--- Euler.py
+++ Euler.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 import getopt
-import numpy as np
+import math
+import numpy
 import re
 import sys
 
@@ -70,7 +71,7 @@ def solve14():
     longest_sequence = (1, 1)
     # Memoize the first 100,000 numbers
     memo_limit = 100000
-    memoized_lengths = np.zeros(100000)
+    memoized_lengths = numpy.zeros(100000)
     for num in xrange(2, 1000000):
         length = get_hotpo_length(num, memoized_lengths)
         #print("Length is %d" % length)
@@ -146,6 +147,55 @@ def solve17():
         result = result + len(tempstring)
     print("\n\nThe answer is: %d" % result)
 
+def convert_triangle_str_to_matrix(triangle_str):
+    numbers = triangle_str.split()
+    # No. of elements is n(n+1)/2 where n is number of rows
+    # So 2y = x^2 + x, or x^2 + x - 2y = 0
+    # Solve x = (-1 + sqrt(1 + 4(2y)))/2
+    num_rows = (-1 + math.sqrt(1 + 8 * len(numbers)))/2
+    # Using zeros as a filler, if the problem has zeros then replace with something else, like -1
+    triangle_mat = numpy.zeros((num_rows, num_rows), dtype=numpy.int)
+    # Start inserting numbers in the matrix
+    cur_row = 0
+    row_count = 0
+    for number in numbers:
+        triangle_mat[cur_row][row_count] = number
+        row_count += 1
+        if row_count == cur_row + 1:
+            row_count = 0
+            cur_row += 1
+    return triangle_mat
+
+triangle_str_prob_18 = """
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+"""
+
+def get_largest_triangle_path(triangle_mat):
+    sum_mat = triangle_mat
+    largest_sum = 0
+    for row in range(1, numpy.shape(sum_mat)[0]):
+        largest_sum += sum_mat[row][0]
+
+    return largest_sum
+
+def solve18():
+    """Find the maximum total from the top of the triangle to the base"""
+    triangle_mat = convert_triangle_str_to_matrix(triangle_str_prob_18)
+    print("Largest sum = %d" % get_largest_triangle_path(triangle_mat))
 
 euler_problems = {
         12: solve12,
@@ -154,13 +204,14 @@ euler_problems = {
         15: solve15,
         16: solve16,
         17: solve17,
+        18: solve18,
         }
 
 def main():
     optlist, args = getopt.getopt(sys.argv[1:], "hp:", ["help", "problem_number"])
     for o, a in optlist:
         if o == "-p":
-           euler_problems[int(a)]()
+            euler_problems[int(a)]()
         else:
             print("Usage: %s -p <problem-number>")
             sys.exit()
